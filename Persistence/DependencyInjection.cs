4ec6a0e3cb43fec48c37721cf4717b150930c5ec
MrPrograMax <grigorievma9461@gmail.com>
2023-05-05 19:01:48 +0700

    Добавлена реализация регистрации групп сервисов

---
+++ Persistence/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Application.Interfaces;
+
+namespace Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<UserManagementDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString);
+            });
+
+            services.AddScoped<IUserManagementDbContext>(provider => provider.GetService<UserManagementDbContext>());
+
+            return services;
+        }
+    }
+}
